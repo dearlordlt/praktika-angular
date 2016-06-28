adcb12ef465e4cd6eef255401032763b4c38eca1
Jurcix <pakalnyte.jurate@gmail.com>
2016-06-28 12:22:00 +0300

    added list

--- app/view5/view5.js
+++ app/view5/view5.js
@@ -13,6 +13,12 @@ angular.module('myApp.view5', ['ngRoute'])
     }])
 
     .controller('View5Ctrl', ['$scope', function($scope){
-        $scope.puikus= 'Mano puikusis view5'
-        $scope.klausimas = 'Ar jums patinka mano view5?'
+        $scope.font =
+        {
+            puikus: 'Mano puikusis view5',
+            klausimas: 'Ar jums patinka mano view5',
+            klausimas2: 'Ar reikia ka nors pakeisti?',
+            listas: ['taip','ne', 'sueis']
+        }
+
     }]);
