26d3a3a74457ac4aabf558e7ad50c612d360d2ff
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-06-29 00:40:07 -0700

    Not fully functional interval fucntion

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -9,12 +9,32 @@ angular.module('myApp.view4', ['ngRoute'])
   });
 }])
 
-.controller('View4Ctrl', ['$scope', function($scope) {
+.controller('View4Ctrl', ['$scope','$interval',function($scope,$interval) {
 	$scope.randomThing = "This will be a cookie clicker game"
 	$scope.cookieAmount = 0;
 	$scope.cookieIncrement = function()
 	{
-		$scope.cookieAmount =+ 1;
+		$scope.cookieAmount += 1;
 		return $scope.cookieAmount;
 	};
+	
+	$scope.cookieAdder = function($interval)
+	{
+		$scope.cookieAdder1()
+		{
+			$scope.cookieAmount += 1;
+		}
+		$interval(cookieAdder1, 1000);
+		return $scope.cookieAmount;
+		
+	};
 	}]);
+	
+	/*$scope.timer =setInterval($scope.cookieAdder(), 1000);
+	$scope.cookieAdder = function()
+	{
+		$scope.cookieAmount += 1;
+	};
+	
+	$scope.a = "yes";
+	}]);*/
