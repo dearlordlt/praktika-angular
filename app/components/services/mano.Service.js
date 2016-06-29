880b7c94ed2a9b88974cc6e9f36299b13a7b79d8
Jurcix <pakalnyte.jurate@gmail.com>
2016-06-29 11:18:09 +0300

    pagaliau veikia

---
+++ app/components/services/mano.Service.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jurcix on 2016-06-28.
+ */
+'use strict';
+
+angular.module('myApp').factory('manoService', function (){
+
+    var kintamasis = 0;
+
+
+    return {
+        kintamasis: kintamasis
+    }
+
+});
--- app/view5/view5.js
+++ app/view5/view5.js
@@ -3,14 +3,21 @@
  */
 'use strict';
 
-angular.module('myApp.view5', ['ngRoute']).controller('View5Ctrl', ['$scope', function ($scope) {
+angular.module('myApp.view5', ['ngRoute']).controller('View5Ctrl',  ['$scope','manoService', function ($scope, manoService ) {
     $scope.font = {
         puikus: 'Mano puikusis view5',
         klausimas: 'Ar jums patinka mano view5?',
         klausimas2: 'Ar reikia ka nors pakeisti?',
         listas: ['taip', 'ne', 'sueis'],
         vardas: 'Jurate'
-    }
+    };
+    $scope.score = manoService.kintamasis;
+
+    $scope.addKintamasis = function() {
+        manoService.kintamasis = $scope.score;
+        $scope.myScore = manoService.kintamasis;
+    };
+
 
 }]);
 
