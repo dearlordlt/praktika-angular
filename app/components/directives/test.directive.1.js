2921f34463f3452c7f36b5a13dbe48f5844fb2c4
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-06-30 21:39:15 -0700

    merge

--- app/components/directives/test.directive.1.js
+++ app/components/directives/test.directive.1.js
@@ -5,7 +5,8 @@ angular.module('myApp').directive('testDirective', function() {
         templateUrl: 'components/directives/templates/test.directive.1.tmpl.html',
         scope: {
             id : '=id',
-            jsonGet : '=jsonGet'
+            jsonGet : '=jsonGet',
+            characters : '=characters'
         },
         controller: function($scope, $http) {
             $http.get(' http://swapi.co/api/films/' + $scope.id + '/').success(function(responce) {
@@ -17,13 +18,16 @@ angular.module('myApp').directive('testDirective', function() {
                     console.log(responce);
                     $scope.starwarsData = responce;
                 });
-                for (var i=0; i<$scope.starwarsData.charecters.length;i++) {
-                    $scope.starwarsData1 = 'loading';
-                    $http.get(' http://swapi.co/api/people/' + 1 + '/').success(function (responce) {
-                        console.log(responce);
-                        $scope.starwarsData1 = responce;
-                    });
-                }
+                $scope.characters = function () {
+                    $scope.i = 0;
+                    for (i = 0; i < $scope.starwarsData.characters.length; i++) {
+                        $scope.starwarsData1 = 'loading';
+                        $http.get(' http://swapi.co/api/people/' + $scope.i + '/').success(function (responce) {
+                            console.log(responce);
+                            $scope.starwarsData1 = responce;
+                        });
+                    }
+                };
 
                    
 
