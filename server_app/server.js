839a3273c10daf1489596cb07c78bfca7a860f33
ssliteris <ssliteris@deloitte.com>
2016-07-03 16:51:47 +0300

    authentification added

--- server_app/server.js
+++ server_app/server.js
@@ -3,22 +3,28 @@
 // BASE SETUP
 // =============================================================================
 
-var express = require('express');       // express
+var express = require('express');           // express
 var app = express();                    // define app
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var mongoose = require('mongoose');
+var config = require('./config');
 var User = require('./models/users');
-var colors = require('colors');         // use colors in console
+var colors = require('colors');            // use colors in console
+var jwt = require('jsonwebtoken');
 
-mongoose.connect('mongodb://localhost/myappdatabase');
+mongoose.connect(config.database);
 
+app.set('superSecret', config.secret);
+
+app.use(morgan('dev'));                             // use morgan to log requests to the console
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 9001;        // port
-
 // ROUTES
 // =============================================================================
+
+var port = process.env.PORT || 9001;                // port
 var router = express.Router();
 
 router.use(function (req, res, next) {
@@ -29,13 +35,42 @@ router.use(function (req, res, next) {
     //Response type
     res.setHeader("Content-Type", "application/json");
 
-    //Log
-    console.log('API CALL: ['.blue + (' "' + req.url + '"').red + ' ]'.blue);
-    next(); // go to the next routes and don't stop here
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    console.log(config.allowedUrls.indexOf(req.url),req.url, config.allowedUrls);
+
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({success: false, message: 'Failed to authenticate token.'});
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else if(config.allowedUrls.indexOf(req.url) === -1) {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    } else {
+        next();
+    }
+
+
 });
 
 /**
- * home route (GET http://localhost:9001/api)
+ * home route (GET http://localhost:9001/api) with instructions
  */
 router.get('/', function (req, res) {
     res.json({
@@ -46,10 +81,16 @@ router.get('/', function (req, res) {
             getUserById: 'GET:       /api/users/:userId  Get a single user.',
             updateUserById: 'PUT:       /api/users/:userId  Update a user with new info.',
             deleteUserById: 'DELETE:    /api/users/:userId  Delete a user.'
+        },
+        authenticate: {
+            authenticate: 'POST:      /api/authenticate - send username and password to get token'
         }
     });
 });
 
+/**
+ * Users rest service
+ */
 router.route('/users')
 // create a users (accessed at POST http://localhost:9001/api/users)
     .post(function (req, res) {
@@ -127,8 +168,8 @@ router.route('/users/:user_id')
                     console.log('ERROR UPDATING USER: ' + err.errmsg.red);
                     res.status(500).json({error: err});
                 } else {
-                    console.log('SUCCESS UPDATING USER'.green + (' id:' + req.params.user_id).red);
-                    res.status(200).json(user);
+                    console.log('SUCCESS UPDATING USER'.green);
+                    res.status(200).json({success: 'User is updated'});
                 }
             });
 
@@ -147,6 +188,52 @@ router.route('/users/:user_id')
             }
         });
     });
+
+/**
+ * Authenticate rest service
+ */
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+router.post('/authenticate', function (req, res) {
+
+    // find the user
+    User.findOne({
+        username: req.body.username
+    }, function (err, user) {
+
+        if (err) {
+            console.log('ERROR AUTH USER: ' + err.errmsg.red);
+            res.status(403).json({error: err});
+            return;
+        }
+
+        if (!user) {
+            res.json({success: false, message: 'Authentication failed. User not found.'});
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({success: false, message: 'Authentication failed. Wrong password.'});
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresIn: 60 * 60 * 24 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Token is set!',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
 
 // REGISTER ROUTES -------------------------------
 // all of our routes will be prefixed with /api
