9831d075342182b07f0739ce9383c399f1997ab6
Jurcix <pakalnyte.jurate@gmail.com>
2016-07-08 12:05:00 +0300

    added proper modals

--- app/users.db.jurates/modals/modals.users.db.js
+++ app/users.db.jurates/modals/modals.users.db.js
@@ -1,16 +1,59 @@
 'use strict';
 
-angular.module('myApp.usersDB').controller('usersDBModal',['$scope', 'items', '$uibModalInstance',
-    function ($scope, items, $uibModalInstance) {
+angular.module('myApp.usersDB').controller('usersDBModal', ['$scope', 'items', '$uibModalInstance', '$http', '$cookies',
+    function ($scope, items, $uibModalInstance, $http, $cookies) {
 
-    $scope.items = items;
+        $scope.items = items;
 
-    $scope.ok = function () {
-        $uibModalInstance.close();
-    };
+        $scope.ok = function () {
+            $uibModalInstance.close();
+        };
 
-    $scope.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
 
-}]);
+
+        $scope.actionPost = function () {
+            console.log($scope.user);
+            $http({
+                method: 'POST',
+                url: 'http://localhost:9001/api/users/?token=' + $cookies.get('login.token'),
+                data: $scope.user
+            }).success(function (response) {
+                $scope.addedUser = response;
+                console.log($scope.addedUser);
+            }).error(function (error) {
+                alert(error);
+            });
+
+        };
+
+        $scope.saveUser = function () {
+         /*   if (!$scope.token) {
+                //error
+            }*/
+            $http({
+                method: 'PUT',
+                url: 'http://localhost:9001/api/users/' + $scope.items.id + '/?token=' + $cookies.get('login.token'),
+                data: {
+                    name: $scope.items.name,
+                    username: $scope.items.username,
+                    password: $scope.items.password,
+                    admin: $scope.items.admin,
+                    location: $scope.items.location
+                }
+            }).success(function (response) {
+                $scope.save = response;
+                console.log($scope.save);
+            });
+        };
+
+        $scope.deleteUser = function () {
+            $http.delete('http://localhost:9001/api/users/' + $scope.items.id + '/?token=' + $cookies.get('login.token')).success(function (response) {
+                $scope.deletedUser = response;
+            });
+        };
+
+
+    }]);
--- app/users.db.jurates/users.db.jurates.js
+++ app/users.db.jurates/users.db.jurates.js
@@ -1,103 +1,41 @@
 angular.module('myApp.usersDB', ['ngRoute']).controller('usersDBCtrl', ['$scope', '$http', '$cookies', '$uibModal', '$log',
     function ($scope, $http, $cookies, $uibModal, $log) {
-    $scope.user = {};
+        $scope.user = {};
 
-    $scope.showInput = false;
+        $scope.showInput = false;
 
-    // ------------------------------- GETing all users -----------------------------------
-    $scope.actionGet = function () {
-        $http.get('http://localhost:9001/api/users/?token=' + $cookies.get('login.token')).success(function (response) {
-            $scope.allUsers = response;
-            console.log($scope.allUsers);
-        });
-    };
+        // ------------------------------- GETing all users -----------------------------------
+        $scope.actionGet = function () {
+            $http.get('http://localhost:9001/api/users/?token=' + $cookies.get('login.token')).success(function (response) {
+                $scope.allUsers = response;
+                console.log($scope.allUsers);
+                $scope.actionGetModal();
+            });
 
-    // -------------------------------- adding user ------------------------------
-    $scope.actionPost = function () {
-        console.log($scope.user);
-        $http({
-            method: 'POST',
-            url: 'http://localhost:9001/api/users/?token=' + $cookies.get('login.token'),
-            data: $scope.user
-        }).success(function (response) {
-            $scope.addedUser = response;
-            console.log($scope.addedUser);
-        }).error(function (error) {
-            alert(error);
-        });
-    };
-
-    //------------------------------- getting user by ID ------------------------------
-    $scope.actionID = function () {
-        $http.get('http://localhost:9001/api/users/' + $scope.user.id + '/?token=' + $cookies.get('login.token')).success(function (response) {
-            $scope.userID = response;
-            console.log($scope.userID);
-        });
-    };
-
-    $scope.showHidden = function () {
-        $scope.showInput = true;
-    };
-
-    $scope.allInOne = function () {
-        $scope.actionID();
-        $scope.showHidden();
-    };
-
-    //-------------------------------------- editing user ----------------------------------
-    $scope.saveUser = function () {
-        if (!$scope.token) {
-            //error
-        }
-        $http({
-            method: 'PUT',
-            url: 'http://localhost:9001/api/users/' + $scope.user.id + '/?token=' + $cookies.get('login.token'),
-            data: {
-                name: $scope.userID.name,
-                username: $scope.userID.username,
-                password: $scope.userID.password,
-                admin: $scope.userID.admin,
-                location: $scope.userID.location
-            }
-        }).success(function (response) {
-            $scope.save = response;
-            console.log($scope.save);
-        });
-    };
-
-    //---------------------------------------- deleting user --------------------------------------------
-    $scope.deleteUser = function () {
-        $http.delete('http://localhost:9001/api/users/' + $scope.user.id + '/?token=' + $cookies.get('login.token')).success(function (response) {
-            $scope.deletedUser = response;
-        });
-    };
+            $scope.actionGetModal = function () {
+                $scope.items = $scope.allUsers;
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'users.db.jurates/modals/modals.users.db.html',
+                    controller: 'usersDBModal',
+                    size: 'lg',
+                    resolve: {
+                        items: function () {
+                            return $scope.items;
+                        }
+                    }
+                });
+            };
+        };
 
 
-    //--------------------------------------- alerts ---------------------------------------------
-    $scope.alerts = {
-        successfulAuthentication: {
-            type: 'success',
-            msg: 'Authentication successful! Please continue'
-        },
-        failedAuthentication: {
-            type: 'danger',
-            msg: 'Authentication failed. Please try again'
-        }
-    };
+        // -------------------------------- adding user ------------------------------
 
-    $scope.closeAlert = function (index) {
-        $scope.alerts.splice(index, 1);
-    };
-
-
-    //------------------------------------- modals -----------------------------------------
-    $scope.items = ['item1', 'item2', 'item3'];
-
-        $scope.showInfo = function () {
+        $scope.actionPostModal = function () {
 
             var modalInstance = $uibModal.open({
                 animation: true,
-                templateUrl: 'users.db.jurates/modals/modals.users.db.html',
+                templateUrl: 'users.db.jurates/modals/modals.users.db2.html',
                 controller: 'usersDBModal',
                 size: 'sm',
                 resolve: {
@@ -107,5 +45,58 @@ angular.module('myApp.usersDB', ['ngRoute']).controller('usersDBCtrl', ['$scope'
                 }
             });
         };
-    }]);
 
+
+        //------------------------------- getting user by ID ------------------------------
+        $scope.actionID = function () {
+            $http.get('http://localhost:9001/api/users/' + $scope.user.id + '/?token=' + $cookies.get('login.token')).success(function (response) {
+                $scope.userID = response;
+                console.log($scope.userID);
+                $scope.actionIDModal();
+            });
+
+        };
+
+        $scope.actionIDModal = function () {
+            $scope.items = $scope.userID;
+            $scope.items.id = $scope.user.id;
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'users.db.jurates/modals/modals.users.db3.html',
+                controller: 'usersDBModal',
+                size: 'lg',
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+
+
+
+                    }
+                }
+            });
+        };
+
+
+
+
+        //--------------------------------------- alerts ---------------------------------------------
+        $scope.alerts = {
+            successfulAuthentication: {
+                type: 'success',
+                msg: 'Authentication successful! Please continue'
+            },
+            failedAuthentication: {
+                type: 'danger',
+                msg: 'Authentication failed. Please try again'
+            }
+        };
+
+        $scope.closeAlert = function (index) {
+            $scope.alerts.splice(index, 1);
+        };
+
+
+        //------------------------------------- modals -----------------------------------------
+
+
+    }]);
