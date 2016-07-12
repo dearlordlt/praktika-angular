8cf1a339c48c91a144f569ef1e82d30875df655e
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-07-12 09:28:18 +0300

    BiscuitClicker

---
+++ app/components/services/biscuit.multiplicator.service.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Richard on 11/07/2016.
+ */
+'use strict'
+
+angular.module('myApp').factory('biscuitMultiplicatorService', function (){
+
+
+    var mult = 0;
+    var typeChecker ='';
+    if(typeChecker === 5){
+        var mult5 = mult * 2;
+    }
+    return {
+        multiplicator : multiplicator
+
+    }
+});
