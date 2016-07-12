9bfdd96e4dfb8cbf339d03a61ad3ed39e1820205
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-07-12 10:41:25 +0300

    merge

--- app/biscuitclicker/biscuitclicker.js
+++ app/biscuitclicker/biscuitclicker.js
@@ -22,18 +22,38 @@ angular.module('myApp.BiscuitClicker', []).controller('BiscuitClickerCtrl', ['$s
                     case 5:
                         $scope.price = (1.05 ^ $scope.adder5Amount) * initPrice;
                         $scope.adder5Amount += 1;
+                        add = $interval(function () {
+                                $scope.biscuitAmount += amount*multiplicator;
+                                return $scope.biscuitAmount;
+                            }
+                            , 1000);
                         break;
                     case 25:
                         $scope.price = (1.05 ^ $scope.adder25Amount) * initPrice;
                         $scope.adder25Amount += 1;
+                        add = $interval(function () {
+                                $scope.biscuitAmount += amount*multiplicator;
+                                return $scope.biscuitAmount;
+                            }
+                            , 1000);
                         break;
                     case 100:
                         $scope.price = (1.05 ^ $scope.adder100Amount) * initPrice;
                         $scope.adder100Amount += 1;
+                        add = $interval(function () {
+                                $scope.biscuitAmount += amount*multiplicator;
+                                return $scope.biscuitAmount;
+                            }
+                            , 1000);
                         break;
                     case 500:
                         $scope.price = (1.05 ^ $scope.adder500Amount) * initPrice;
                         $scope.adder500Amount += 1;
+                        add = $interval(function () {
+                                $scope.biscuitAmount += amount*multiplicator;
+                                return $scope.biscuitAmount;
+                            }
+                            , 1000);
                         break;
                 }
             };
@@ -41,13 +61,16 @@ angular.module('myApp.BiscuitClicker', []).controller('BiscuitClickerCtrl', ['$s
                 $scope.typeChecker();
                 $scope.biscuitAmount = $scope.biscuitAmount - $scope.price;
 
-                    add = $interval(function () {
-                        $scope.biscuitAmount += amount*multiplicator;
-                        return $scope.biscuitAmount;
-                    }
-                    , 1000);
+
 
             }
+        };
+        $scope.biscuitUpgrade = function(){
+            $scope.multiplicator = 1;
+            $scope.multiplicator=biscuitMultiplicatorService.multiplicator;
+            ;
+
+
         };
         /*function biscuitAdderObj (type, price, addingPower, multiplicator){
             this.type = type;
--- app/components/services/biscuit.multiplicator.service.js
+++ app/components/services/biscuit.multiplicator.service.js
@@ -6,11 +6,8 @@
 angular.module('myApp').factory('biscuitMultiplicatorService', function (){
 
 
-    var mult = 0;
-    var typeChecker ='';
-    if(typeChecker === 5){
-        var mult5 = mult * 2;
-    }
+    var multiplicator = 0;
+   multiplicator = multiplicator * 2;
     return {
         multiplicator : multiplicator
 
